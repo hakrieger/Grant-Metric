87baa253132976ba93cb0a1e051551467e250e59
Heather Krieger <heather.a.krieger@gmail.com>
2018-06-20 18:29:37 -0400

    Updating file to most recent version of 06/29/18

--- Grantpubsearch.py
+++ Grantpubsearch.py
@@ -1,128 +1,87 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 12 10:53:37 2017
+Using Grant Numbers to get PMIDS of published papers
 
-@author: hkrieger
+Created on Thu Apr  5 15:05:23 2018
+
+@author: hk
 """
 import os
 os.getcwd()
-
-
-"""
-Using Grant Numbers to get PMIDS of published papers
-"""
+os.chdir('/Users/hk/Dropbox/TDI/Project')
 
 import requests, time
 from bs4 import BeautifulSoup as bs
+import pandas as pd
+import numpy as np
 
-gpubc = open('gpubcount2.txt', 'w')
-gpubids = open('gpubids2.txt','w')
-fpmids = open('pmidsonly2.txt','w')
+#importing NIH datafile
+df = pd.read_csv('RePORTER_PRJ_C_FY2004.csv')
+df['gnum'] = df['CORE_PROJECT_NUM'].str[3:] #parsing to get just the grant num
+df['mech'] = df['CORE_PROJECT_NUM'].str[0:3] #parsing the mechanism
 
+#removing duplicate grant ID numbers
+gnum = np.unique(np.array(df.gnum))
+len(gnum)
+
+#gnum1 = gnum[0:27985]
+#gnum2 = gnum[27985:55970]
+#gnum3 = gnum[400:]
+
+#assigning url information
+urlmain1 = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=5000&email=heather.a.krieger@gmail.com&term="
+urltail1 = "[Grant%1000Number]&api_key="
+apikey = "87d16f20bd554d28372d7d9e4003ea03d508"
+
+#creating empty lists to append gathered data to
 pcount = []
-grant = []
-apmid = []
+idarray = []
+gnarray = []
 
+#loop to collect data
+j=0
 start = time.time()
-loopnum = 1
+while j < len(gnum):
 
-urlmain1 = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=1000&term="
-urltail1 = "[Grant%1000Number]"
-for number1 in ["AA014576",'AA024377']:
-    url1 = urlmain1+str(number1)+urltail1
-
+    url1 = urlmain1+gnum[j]+urltail1+apikey
     page1 = requests.get(url1)
     soup1 = bs(page1.content, 'html.parser')
 
-    pubcount = soup1.find('count')
-    pubc = pubcount.get_text()
-    pcount.append(pubc)
-    grant.append(number1)
+    try:
+        pubcount = soup1.find('count')
+        pubc = pubcount.get_text()
+        pcount.append(pubc)
+    except:
+        pcount.append('0')
 
-    pmids = soup1.find_all('id')
-    idarray = []
-    #gnum = []
+    #print(df.gnum[j], pcount[j])
+    try:
+        pmids = soup1.find_all('id')
+        idar = []
+        if pmids != []:
+            for idnum in pmids:
+                idar.append(idnum.get_text())
+                gnarray.append(gnum[j])
+            idarray = idarray + idar
+        else:
+            idarray.append('0')
+            gnarray.append(gnum[j])
+    except:
+        idarray.append('0')
+        gnarray.append(gnum[j])
+#    print(gnarray, idarray)
+    #time.sleep(.001)
+    j = j+1
+end = time.time()
+diff = end-start
+print(diff)
 
-    for idnum in pmids:
-      idarray.append(idnum.get_text())
-    m=0
-
-    while m <= (len(idarray)-1):
-        gpubids.write(str(number1)+' '+str(idarray[m])+'\n')
-        fpmids.write(str(idarray[m])+'\n')
-        print(time.time(),number1,idarray[m])
-        m=m+1
-
-    if(loopnum <= 10):  #restricting the number of requests to 10
-        loopnum = loopnum +1
-    else:
-        diff = (time.time()-start)
-        time.sleep(1-diff)      #forcing a pause for only 10 requests a second
-        start = time.time()
-        loopnum = 1
-
-
-n=0
-while n <= (len(grant)-1):
-    gpubc.write(str(n+1)+' '+str(grant[n])+' '+str(pcount[n])+'\n')
-    #print(n+1,grant[n],pcount[n])
-    n=n+1
-
-gpubc.close()
-gpubids.close()
-fpmids.close()
+pubcounts = pd.DataFrame({'gid' : gnum,
+                       'pubcount' : pd.Series(pcount)})
+pubcounts.to_csv('gnum2004-1.csv', sep = ',', index = False)
 
 
-"""
-Using PMIDS to get cited by counts
-"""
-#fpmids2 = open('fpmids1.txt','r') #pmidsonly.txt
-#for line in fpmids2:
-#    number2.append(line.strip('\n'))
-
-import time
-
-urlmain2 = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&linkname=pubmed_pmc_refs&id="
-
-citecount = open('citecount.txt', 'w')
-pubid = []
-cites = []
-
-start = time.time()
-loopnum = 1
-
-for number in [29097835, 28580226, 28548347, 27588833, 27260007, 27030295, 29226004, 29097835, 28772247, 28580226]:
-    url = urlmain2+str(number)
-    page2 = requests.get(url)
-    soup2 = bs(page2.content, 'html.parser')
-    ids = soup2.find_all('id')
-    aid = ids[0].get_text()
-    count = len(ids)-1
-    aidr=str(aid)
-    countr=str(count)
-    pubid.append(aidr)
-    cites.append(countr)
-
-
-    if(loopnum <= 10):  #restricting the number of requests to 10
-        loopnum = loopnum +1
-    else:
-        diff = (time.time()-start)
-        time.sleep(1-diff)      #forcing a pause for only 10 requests a second
-        start = time.time()
-        loopnum = 1
-
-
-m=0
-while m <= (len(pubid)-1):
-    citecount.write(str(m+1)+' '+str(pubid[m])+' '+str(cites[m])+'\n')
-    print(m+1,time.asctime( time.localtime(time.time()) ),pubid[m],cites[m])
-    m=m+1
-
-citecount.close()
-
-
-
-
-
-
+pmidsforcites = pd.DataFrame({'gid' : pd.Series(gnarray),
+                       'pmids' : pd.Series(idarray)})
+pmidsforcites.to_csv('gnum2004-2.csv', sep = ',', index= False)
